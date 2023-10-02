694776c9005c1ceeb5d54347d6052996221ce886
Dennis Jackson <dennisdjack1987@gmail.com>
2023-10-01 23:57:49 -0700

    bug fixes

--- server/server.js
+++ server/server.js
@@ -23,9 +23,9 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(process.cwd(), "../client/build")));
   }
  
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
+//   app.get("/*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
+//   });
 
 app.use('/api',require('./routes/api'))
 const server = http.createServer(app);
